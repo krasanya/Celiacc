a886d272465d452b51137a557e6554f8ede709d0
krasanya <annakr@post.bgu.ac.il>
2018-03-16 12:24:40 +0200

    mainActivityButtons

--- app/src/main/java/il/non/celiacc/MainActivity.java
+++ app/src/main/java/il/non/celiacc/MainActivity.java
@@ -1,7 +1,12 @@
 package il.non.celiacc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText etUsername = {EditText}  findViewById(R.id.etUsername);
+        final EditText etPassword = {EditText} findViewById(R.id.etPassword);
+        final Button btLogin = {Button} findViewById(R.id.btLogin);
+        final TextView tvNewUser = {TextView} findViewById(R.id.tvNewUser);
+
+        tvNewUser.setOnClickListener(new  View.OnClickListener(){
+            @Override
+                    public void onClick(View v){
+                Intent registerIntent = new Intent(MainActivity.this , NewUserActivity.class);
+                MainActivity.this.startActivity(registerIntent);
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/il/non/celiacc/NewUserActivity.java
+++ app/src/main/java/il/non/celiacc/NewUserActivity.java
@@ -2,6 +2,9 @@ package il.non.celiacc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class NewUserActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class NewUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+
+        final EditText etEmail = {EditText}  findViewById(R.id.etEmail);
+        final EditText etUsername = {EditText}  findViewById(R.id.etUsername);
+        final EditText etFirstname = {EditText}  findViewById(R.id.etFirstname);
+        final EditText etLastname = {EditText}  findViewById(R.id.etLastname);
+        final EditText etPass = {EditText} findViewById(R.id.etPass);
+        final CheckBox cbIsmember = {CheckBox} findViewById(R.id.cbIsmember);
+        final Button btRegister = {btRegister} findViewById(R.id.btRegister);
     }
 }
